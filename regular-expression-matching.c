0a57a7e3a7e1be846a1e87b5df04609c0dd6e3d4
ruizhang331 <ruizhang331@gmail.com>
2016-08-01 23:33:17 -0700

    Update regular-expression-matching.c

--- regular-expression-matching.c
+++ regular-expression-matching.c
@@ -24,12 +24,11 @@ dp[i][j] indicates whether substring s(0:i-1) matches substring p(0:j-1)
 1, If p[j-1] == s[i-1] :  dp[i][j] = dp[i-1][j-1];
 2, If p[j-1] == '.' : dp[i][j] = dp[i-1][j-1];
 3, If p[j-1] == '*': we need to check p[j-2] as it is precedening char
-   here are two sub conditions:
-     1   if p[j-2] != s[i-1] : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
-     2   if p[j-2] == s[i-1] or p[j-2] == '.':
-                    dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a 
-                 or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
-                 or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+    3.1 if * matches 0 precedent element p[j-2],so p[0: j-1] = p[0: j-3]. so dp[i][j]=dp[i][j-2]
+    3.2 if * matches 1 precedent element p[j-2],so p[0: j-1] = p[0: j-2]. dp[i][j]=dp[i][j-1]
+    3.2 if * matches many precedent element p[j-2], so p[0: j-1] = {p[0: j-2],...,p[0: j-2]}
+       dp[i][j]=dp[i-1][j] if p[j-2]==s[i-1]||p[j-2] == '.'
+       dp[i][j]=false otherwise 
 
 */
 
@@ -37,37 +36,33 @@ dp[i][j] indicates whether substring s(0:i-1) matches substring p(0:j-1)
 class Solution {
 public:
   bool isMatch(string s, string p) {
+        int len_s = s.size();
+        int len_p = p.size();
 
-      int len_s = s.size();
-      int len_p = p.size();
-      
-
-      vector<vector<bool>> dp(len_s+1,vector<bool>(len_p+1,false));
-      dp[0][0] = true;
-      
-      
-      for (int j= 2; j<=len_p; j++){
-          if(p[j-1]=='*' && dp[0][j-2])
-              dp[0][j] = true;
-      }
-      
-      for(int i=1;i<=len_s;i++){
-          for(int j=1;j<=len_p;j++){
-              if(s[i-1]==p[j-1] || p[j-1]=='.'){
-                  dp[i][j] = dp[i-1][j-1];
-              }
-              if(p[j-1]=='*'){
-                  //need to check precedent char: p[j-2]
-                  if(p[j-2]==s[i-1]||p[j-2] == '.')
-                      dp[i][j] = dp[i-1][j] || dp[i][j-1] ||dp[i][j-2];
-                  else if(p[j-2]!=s[i-1])
-                      dp[i][j] = dp[i][j-2];
-                      
-              }
-          }
-      }
-          
-      return dp[len_s][len_p];
+        vector<vector<bool>> dp(len_s+1,vector<bool>(len_p+1,false));
+        dp[0][0] = true;
+        //dp[0][1] = true;
+        
+        for (int j= 2; j<=len_p; j++){
+            if(p[j-1]=='*' && dp[0][j-2])
+                dp[0][j] = true;
+        }
+        
+        for(int i=1;i<=len_s;i++){
+            for(int j=1;j<=len_p;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='.'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                if(p[j-1]=='*'){
+                    dp[i][j] = dp[i][j-2] ||//* matches 0 precedent element p[j-2]
+                    dp[i][j-1] ||//* matches 1 precedent element p[j-2]
+                    (dp[i-1][j]&&(p[j-2]==s[i-1]||p[j-2] == '.')); //* matches many precedent element p[j-2]
+                        
+                }
+            }
+        }
+            
+        return dp[len_s][len_p];
   }
 };
 
