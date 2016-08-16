70e69e4568e77561aaec8574e6f831bfe370e8a1
ruizhang331 <ruizhang331@gmail.com>
2016-08-15 22:54:55 -0700

    Update number-of-islands.c

--- number-of-islands.c
+++ number-of-islands.c
@@ -35,7 +35,7 @@ int numIslands(vector<vector<char>>& grid) {
     }
     return ret;
 }
-
+    
 void DFS_visit(vector<vector<char>>& grid, int x, int y, int m, int n){
     grid[x][y] = 2;//marked all adjcent cells of (x,y) as one island and visited(2 instead of 1)
     if (x + 1 < m && grid[x + 1][y] == '1') {
@@ -55,55 +55,54 @@ void DFS_visit(vector<vector<char>>& grid, int x, int y, int m, int n){
 //method 2: BFS
 class Solution {
 public:
-  //use BFS
-  int numIslands(vector<vector<char>>& grid) {
-      
-      int m = grid.size();
-      if(m==0)
-          return 0;
-      int n = grid[0].size();
-      int ret = 0;
-      vector<vector<bool>> visited(m, vector<bool>(n, false));
-      queue<int> q_row;
-      queue<int> q_col;
+//use BFS
+int numIslands(vector<vector<char>>& grid) {
+  
+  int m = grid.size();
+  if(m==0)
+      return 0;
+  int n = grid[0].size();
+  int ret = 0;
+  vector<vector<bool>> visited(m, vector<bool>(n, false));
+  queue<int> q_row;
+  queue<int> q_col;
 
-      for(int i=0;i<m;i++){
-          for(int j=0;j<n;j++){
-              if(grid[i][j]=='1' && visited[i][j]==false){
-                  visited[i][j]==true;
-                  q_row.push(i);
-                  q_col.push(j);
-                  ret++;
-                  while(!q_row.empty()){
-                      int x = q_row.front();
-                      int y = q_col.front();
-                      q_row.pop();
-                      q_col.pop();
-                      if (x+1<m && grid[x+1][y]=='1' && !visited[x+1][y]){
-                          q_row.push(x+1);
-                          q_col.push(y);
-                          visited[x+1][y] = true;
-                      }
-                      if (x-1 >=0 && grid[x-1][y]=='1' && !visited[x-1][y]){
-                          q_row.push(x-1);
-                          q_col.push(y);
-                          visited[x-1][y] = true;
-                      }
-                      if (y+1<n && grid[x][y+1]=='1' && !visited[x][y+1]){
-                          q_row.push(x);
-                          q_col.push(y+1);
-                          visited[x][y+1] = true;
-                      }
-                      if (y-1>=0 && grid[x][y-1]=='1' && !visited[x][y-1]){
-                          q_row.push(x);
-                          q_col.push(y-1);
-                          visited[x][y-1]=true;
-                      }
+  for(int i=0;i<m;i++){
+      for(int j=0;j<n;j++){
+          if(grid[i][j]=='1' && visited[i][j]==false){
+              visited[i][j]==true;
+              q_row.push(i);
+              q_col.push(j);
+              ret++;
+              while(!q_row.empty()){
+                  int x = q_row.front();
+                  int y = q_col.front();
+                  q_row.pop();
+                  q_col.pop();
+                  if (x+1<m && grid[x+1][y]=='1' && !visited[x+1][y]){
+                      q_row.push(x+1);
+                      q_col.push(y);
+                      visited[x+1][y] = true;
+                  }
+                  if (x-1 >=0 && grid[x-1][y]=='1' && !visited[x-1][y]){
+                      q_row.push(x-1);
+                      q_col.push(y);
+                      visited[x-1][y] = true;
+                  }
+                  if (y+1<n && grid[x][y+1]=='1' && !visited[x][y+1]){
+                      q_row.push(x);
+                      q_col.push(y+1);
+                      visited[x][y+1] = true;
+                  }
+                  if (y-1>=0 && grid[x][y-1]=='1' && !visited[x][y-1]){
+                      q_row.push(x);
+                      q_col.push(y-1);
+                      visited[x][y-1]=true;
                   }
               }
           }
       }
-      return ret;
   }
-
+  return ret;
+}
 };
