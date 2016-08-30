ee5d996ff59a1e3686f54da3b5bef772f0a25aec
ruizhang331 <ruizhang331@gmail.com>
2016-08-29 21:55:39 -0700

    Create power-of-three.c

---
+++ power-of-three.c
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/power-of-three/
+//Given an integer, write a function to determine if it is a power of three. 
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1)
+            return true;
+        if(n<=0)
+            return false;
+            
+        while(n%3==0){
+            n = n/3;
+            if(n==1)
+                return true;
+        }
+        return false;
+    }
+};
