3cb728b80301c117d2089a73e2992c6ba4af4b94
ruizhang331 <ruizhang331@gmail.com>
2016-09-02 21:31:37 -0700

    Update remove-duplicates-from-sorted-array.c

--- lintcode/remove-duplicates-from-sorted-array.c
+++ lintcode/remove-duplicates-from-sorted-array.c
@@ -1,4 +1,4 @@
-http://www.lintcode.com/problem/remove-duplicates-from-sorted-array/
+//http://www.lintcode.com/problem/remove-duplicates-from-sorted-array/
 /*
 Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length. 
 Do not allocate extra space for another array, you must do this in place with constant memory. 
@@ -20,7 +20,6 @@ public:
                 nums[++start] = nums[i];
             }
         }
-        
         return start+1;
     }
 };
