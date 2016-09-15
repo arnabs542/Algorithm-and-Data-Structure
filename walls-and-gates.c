4b50c4c856388795eab63cf73965fcd05b8a646a
ruizhang331 <ruizhang331@gmail.com>
2016-09-15 16:21:55 -0700

    Update walls-and-gates.c

--- walls-and-gates.c
+++ walls-and-gates.c
@@ -74,3 +74,47 @@ void DFS(vector<vector<int>>& rooms, int i, int j, int path){
     }
     
 }
+
+
+//BFS way
+class Solution {
+    int row = 0;
+    int col = 0;
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        row=rooms.size();
+        if(!row) return;
+        col=rooms[0].size();
+        if(!col) return;
+        queue<pair<int,int>> q;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(rooms[i][j]==0){
+                    q.push(make_pair(i,j));
+                }
+            }
+        }
+        //BFS
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
+            if(x-1>=0 && rooms[x-1][y]==INT_MAX){
+                rooms[x-1][y] = rooms[x][y]+1;
+                q.push(make_pair(x-1,y));
+            }
+            if(x+1<row && rooms[x+1][y]==INT_MAX){
+                rooms[x+1][y] = rooms[x][y]+1;
+                q.push(make_pair(x+1,y));
+            }
+            if(y-1>=0 && rooms[x][y-1]==INT_MAX){
+                rooms[x][y-1] = rooms[x][y]+1;
+                q.push(make_pair(x,y-1));
+            }
+            if(y+1<col && rooms[x][y+1]==INT_MAX){
+                rooms[x][y+1] = rooms[x][y]+1;
+                q.push(make_pair(x,y+1));
+            }
+        }
+    }
+};
