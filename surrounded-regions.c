2c38b5a575d33f82f8ae4fdd674b79daee9f558e
ruizhang331 <ruizhang331@gmail.com>
2016-09-15 14:25:18 -0700

    Update surrounded-regions.c

--- surrounded-regions.c
+++ surrounded-regions.c
@@ -16,20 +16,63 @@ X X X X
 X X X X
 X O X X
 */
+
+//Method 1: DFS
+class Solution {
+    int row = 0;
+    int col = 0;
+public:
+    void solve(vector<vector<char>>& board) {
+        int i,j;
+        row=board.size();
+        if(!row) return;
+        col=board[0].size();
+        if(!col) return;
+		for(j=0;j<col;j++){
+			DFS(board,0,j);
+			DFS(board,row-1,j);
+		}
+		for(i=0;i<row;i++){
+			DFS(board,i,0);
+			DFS(board,i,col-1);
+		}
+
+		for(i=0;i<row;i++)
+			for(j=0;j<col;j++)
+				if(board[i][j]=='O')
+					board[i][j]='X';
+				else if(board[i][j]=='1')
+					board[i][j]='O';
+    }
+    
+    void DFS(vector<vector<char>>& board, int x, int y){
+		if(board[x][y]=='O'){
+			board[x][y]='1';
+			if(x>1)
+				DFS(board,x-1,y);
+			if(y>1)
+				DFS(board,x,y-1);
+			if(x+1<row)
+				DFS(board,x+1,y);
+			if(y+1<col)
+				DFS(board,x,y+1);
+        }
+    }
+};
+
+//Method 2: BFS
 class Solution {
 public:
   void solve(vector<vector<char>>& board) {
       /*
       The algorithm is quite simple: Use BFS starting from 'O's on the boundary and mark them as 'V', then iterate over the whole board and mark 'O' as 'X' and 'V' as 'O'.
       */
-      
       int row = board.size();
       if (row == 0) //Add this to prevent run-time error!
           return;
       int col = board[0].size();
       if  (col == 0) // Add this to prevent run-time error!
           return;
-  
       for (int i = 0; i < col; ++i)
       {
           if (board[0][i] == 'O')
