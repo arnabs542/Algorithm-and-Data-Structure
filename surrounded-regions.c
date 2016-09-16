4766cb702b24ba21d070068c2720ce6c63b902e9
ruizhang331 <ruizhang331@gmail.com>
2016-09-15 23:58:19 -0700

    Update surrounded-regions.c

--- surrounded-regions.c
+++ surrounded-regions.c
@@ -62,72 +62,69 @@ public:
 
 //Method 2: BFS
 class Solution {
+    int row = 0;
+    int col = 0;
 public:
-  void solve(vector<vector<char>>& board) {
+	void solve(vector<vector<char>>& board) {
       /*
       The algorithm is quite simple: Use BFS starting from 'O's on the boundary and mark them as 'V', then iterate over the whole board and mark 'O' as 'X' and 'V' as 'O'.
       */
       int row = board.size();
-      if (row == 0) //Add this to prevent run-time error!
-          return;
+      if (row == 0) return;
       int col = board[0].size();
-      if  (col == 0) // Add this to prevent run-time error!
-          return;
-      for (int i = 0; i < col; ++i)
-      {
-          if (board[0][i] == 'O')
-              BFS(board, 0, i);
-  
-          if (board[row-1][i] == 'O')
-              BFS(board, row-1, i);
+        queue<pair<int,int>> q;
+
+      for (int i = 0; i < col; ++i){
+          if (board[0][i] == 'O'){
+              q.push(make_pair(0,i));
+              board[0][i] ='V';
+          }
+          if (board[row-1][i] == 'O'){
+              q.push(make_pair(row-1,i));
+              board[row-1][i] ='V';
+          }
       }
   
-      for (int i = 0; i < row; ++i)
-      {
-          if (board[i][0] == 'O')
-              BFS(board, i, 0);
-          if (board[i][col-1] == 'O')
-              BFS(board, i, col-1);
+      for (int i = 0; i < row; ++i){
+          if (board[i][0] == 'O'){
+              q.push(make_pair(i,0));
+              board[i][0] ='V';
+          }
+          if (board[i][col-1] == 'O'){
+              q.push(make_pair(i,col-1));
+              board[i][col-1] ='V';
+          }
+      }
+      
+      while(!q.empty()){
+          int x=q.front().first;
+          int y=q.front().second;
+          q.pop();
+          if(x-1>=0 && board[x-1][y]=='O'){
+              board[x-1][y]='V';
+              q.push(make_pair(x-1,y));
+          }
+          if(x+1<row && board[x+1][y]=='O'){
+              board[x+1][y]='V';
+              q.push(make_pair(x+1,y));
+          }
+          if(y-1>=0 && board[x][y-1]=='O'){
+              board[x][y-1]='V';
+              q.push(make_pair(x,y-1));
+          }
+          if(y+1<col && board[x][y+1]=='O'){
+              board[x][y+1]='V';
+              q.push(make_pair(x,y+1));
+          }
       }
   
-      for (int i = 0; i < row; ++i)
-      {
-          for (int j = 0; j < col; ++j)
-          {
+      for (int i = 0; i < row; ++i){
+          for (int j = 0; j < col; ++j){
               if (board[i][j] == 'O')
                   board[i][j] = 'X';
               else if (board[i][j] == 'V')
                   board[i][j] = 'O';
           }
       }
-      
-      
-  }
-  
-  void BFS(vector<vector<char>>& board, int x, int y){
-      int row = board.size();
-      int col = board[0].size();
-      queue<pair<int,int>> q;
-      q.push(make_pair(x,y));
-      board[x][y]= 'V';
-      while(!q.empty()){
-          pair<int,int> cur = q.front();
-          q.pop();
-          vector<pair<int,int>> adj;
-          adj.push_back(make_pair(cur.first+1,cur.second));
-          adj.push_back(make_pair(cur.first-1,cur.second));
-          adj.push_back(make_pair(cur.first,cur.second+1));
-          adj.push_back(make_pair(cur.first,cur.second-1));
-          for(int i=0;i<adj.size();i++){
-              int adj_x = adj[i].first;
-              int adj_y = adj[i].second;
-              if((adj_x>=0)&&(adj_x<row)&&(adj_y<col)&&(adj_y>=0)){
-                  if(board[adj_x][adj_y]=='O'){
-                      q.push(make_pair(adj_x,adj_y));
-                      board[adj_x][adj_y]= 'V';
-                  }
-              }
-          }
-      }
-  }
+    }
 };
