633727421dc29d77f32debe7fa99ffc3db556aa4
ruizhang331 <ruizhang331@gmail.com>
2016-09-28 22:16:51 -0700

    Create count-of-range-sum.c

---
+++ count-of-range-sum.c
@@ -0,0 +1,52 @@
+//https://leetcode.com/problems/count-of-range-sum/
+/*
+ Given an integer array nums, return the number of range sums that lie in [lower, upper] inclusive.
+Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j (i ≤ j), inclusive.
+
+Note:
+A naive algorithm of O(n2) is trivial. You MUST do better than that.
+
+Example:
+Given nums = [-2, 5, -1], lower = -2, upper = 2,
+Return 3.
+The three ranges are : [0, 0], [2, 2], [0, 2] and their respective sums are: -2, -1, 2. 
+*/
+
+
+
+class Solution {
+public:
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        int size=nums.size();
+        if(size==0)  return 0;
+        vector<long> sums(size+1, 0);
+        for(int i=0; i<size; i++)  sums[i+1]=sums[i]+nums[i];
+        return help(sums, 0, size+1, lower, upper);
+    }
+    
+    /*** [start, end)  ***/
+    int help(vector<long>& sums, int start, int end, int lower, int upper){
+        /*** only-one-element, so the count-pair=0 ***/
+        if(end-start<=1)  return 0;
+        int mid=(start+end)/2;
+        int count=help(sums, start, mid, lower, upper)
+                + help(sums, mid, end, lower, upper);
+        
+        int m=mid, n=mid, t=mid, len=0;
+
+        vector<long> cache(end-start, 0);
+        for(int i=start, s=0; i<mid; i++, s++){
+            
+            while(m<end && sums[m]-sums[i]<lower) m++;
+            while(n<end && sums[n]-sums[i]<=upper) n++;
+            count+=n-m;
+            /*** cache will merge-in-the-smaller-part-of-list2 ***/
+            while(t<end && sums[t]<sums[i]) cache[s++]=sums[t++];
+            cache[s]=sums[i];
+            len=s;
+        }
+        
+        for(int i=0; i<=len; i++)  sums[start+i]=cache[i];
+        return count;
+    }
+};
