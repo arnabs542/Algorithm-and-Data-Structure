8671fc7a376fdde9924ab9ad5c5132673d64cbaf
ruizhang331 <ruizhang331@gmail.com>
2016-09-30 00:23:29 -0700

    Create kth-smallest-element-in-a-bst.c

---
+++ kth-smallest-element-in-a-bst.c
@@ -0,0 +1,57 @@
+//https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+/*
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+*/
+//method 1: ierative
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        //in order traversal
+        stack<TreeNode*> s;
+        int cnt = 0;
+        int ret = 0;
+        while(true){
+            while(root){
+                s.push(root);
+                root=root->left;
+            }
+            cnt++;
+            root = s.top()->right;
+            
+            if(cnt==k){
+                break;
+            }
+            s.pop();
+            
+        }
+        return s.top()->val;
+        
+    }
+};
+
+//recursive
+class Solution {
+private:
+    int cnt=0;
+    int num;
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        cnt = k;
+        help(root);
+        return num;
+    }
+    
+    void help(TreeNode* root){
+        if(cnt>0){
+            if(root->left)
+                help(root->left);
+            cnt--;
+            if(cnt==0){
+                num = root->val;
+                return;
+            }
+            if(root->right)
+                help(root->right);  
+        }
+    }
+};
