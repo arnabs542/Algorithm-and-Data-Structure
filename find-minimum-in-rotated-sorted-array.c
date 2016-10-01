f5c8af7b672f534e7aec5a525f2f26b54f6f7797
ruizhang331 <ruizhang331@gmail.com>
2016-09-30 23:11:49 -0700

    Update find-minimum-in-rotated-sorted-array.c

--- find-minimum-in-rotated-sorted-array.c
+++ find-minimum-in-rotated-sorted-array.c
@@ -8,55 +8,49 @@ Find the minimum element.
 //it is clearly a binary search
 class Solution {
 public:
-    int findMin(vector<int> &num) {
-        int begin = 0;
-        int len = num.size();
-        int end = len-1;
-        int ret = INT_MAX;
-        
-        while(begin<=end){
-            int mid = begin + (end-begin)/2;
-            if(num[mid]>num[end]){
-                begin = mid+1;
-                ret = num[end]<ret?num[end]:ret;
-            }else if(num[mid]<num[end]){
-                end = mid-1;
-                ret = num[mid]<ret?num[mid]:ret;
-            }else{
-                ret = num[mid]<ret?num[mid]:ret;
-                break;
-            }
-        }
-        
-        return ret;
-    }
+	int findMin(vector<int> &num) {
+		int begin = 0;
+		int len = num.size();
+		int end = len - 1;
+		int ret = INT_MAX;
+
+		while (begin<end) {
+			int mid = begin + (end - begin) / 2;
+			if (num[mid]>num[end]) {
+				begin = mid + 1;
+			}
+			else if (num[mid]<num[end]) {
+				end = mid;
+			}
+		}
+
+		return num[begin];
+	}
 };
 
 //follow up, what if there could be equal.
 //https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
 class Solution {
 public:
-    int findMin(vector<int> &num) {
+	int findMin(vector<int>& nums) {
+		int begin = 0;
+		int len = nums.size();
+		int end = len - 1;
 
-        int begin = 0;
-        int end = num.size()-1;
-        int ret = num[0];
-        
-        while(begin<=end){
-            
-            int mid = begin + (end-begin)/2;
-            if(begin < mid && mid < end && num[begin] == num[mid] && num[end] == num[mid]){
-                begin++;
-                end--;
-            }else if(num[mid]<=num[end]){
-                end = mid-1;
-                ret = num[mid]<ret?num[mid]:ret;
-            }else if(num[mid]>num[end]){
-                begin = mid+1;
-                ret = num[end]<ret?num[end]:ret;
-            }
-        }
-            
-        return ret;
-    }
+		while (begin<end) {
+			int mid = begin + (end - begin) / 2;
+			if (nums[mid]>nums[end]) {
+				begin = mid + 1;
+			}
+			else if (nums[mid]<nums[end]) {
+				end = mid;
+			}
+			else {
+				//num[mid] == num[hi], we couldn't sure the position of minimum in mid's left or right, so reduce one high
+				end--;
+			}
+		}
+
+		return nums[begin];
+	}
 };
