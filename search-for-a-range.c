bc9cb6400d9999df203cd21ddada4b6bad63d168
ruizhang331 <ruizhang331@gmail.com>
2016-09-30 23:39:22 -0700

    Update search-for-a-range.c

--- search-for-a-range.c
+++ search-for-a-range.c
@@ -51,3 +51,44 @@ public:
       return ret;
   }
 };
+
+//a more effient way is to search the left then right
+//https://leetcode.com/problems/search-for-a-range/
+/*
+Given a sorted array of integers, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+For example,
+Given [5, 7, 7, 8, 8, 10] and target value 8,
+return [3, 4]. 
+*/
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = 0, end = nums.size(), mid, left, right;
+        while (start < end) {
+            mid = (start + end) / 2;
+            if (nums[mid] >= target)
+                end = mid;
+            else
+                start = mid + 1;
+        }
+        //found the left in range, use this as start point
+        left = start;
+        start = left, end = nums.size();
+        while (start < end) {
+            mid = (start + end) / 2;
+            if (nums[mid] > target)
+                end = mid;
+            else
+                start = mid + 1;
+        }
+        //found the right
+        right = start;
+        return left == right ? vector<int> {-1,-1} : vector<int> {left,right-1};
+    }
+};
