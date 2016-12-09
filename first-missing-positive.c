377cc69269b15145b955f60e075ab851bd536db6
ruizhang331 <ruizhang331@gmail.com>
2016-12-08 17:43:36 -0800

    Update first-missing-positive.c

--- first-missing-positive.c
+++ first-missing-positive.c
@@ -46,3 +46,32 @@ done:
         return ret;
     }
 };
+
+//method 2: put the value to its index
+int firstMissingPositive(vector<int>& nums) {
+/*
+ideally we should have vector as 1,2,3,...n. so nums[i-1]=i
+
+Just go through the array sequentially and for every index write the value at the index to the index specified by value, 
+recursively placing any value at that location to its place and throwing away values > N. 
+Then go again through the array looking for the spot where value doesn't match the index - that's the smallest value not in the array. 
+*/
+    int len = nums.size();
+    int ret = 0;
+    for(int i=0;i<len;i++){
+        int target = nums[i];
+        //recursively placing any value at that location to its place and throwing away values > N
+        while(target>0 && target<=len && target!=nums[target-1]){ //index starts from 0, so target-1 is real index
+            swap(target,nums[target-1]);
+        }
+    }
+
+    for(int i=0;i<len;i++){
+        if(nums[i]!=i+1){
+            ret = i+1;
+            return ret;
+        }
+    }
+return len+1;
+
+}
